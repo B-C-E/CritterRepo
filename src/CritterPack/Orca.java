b181e5485bf5b93ee88a692c40da3fbdd727380e
B-C-E <55931539+B-C-E@users.noreply.github.com>
2019-10-14 13:41:02 -0700

    Cleaned up code, improved Orca
    added random variation

--- src/CritterPack/Orca.java
+++ src/CritterPack/Orca.java
@@ -4,19 +4,20 @@ import java.awt.*;
 
 public class Orca extends Critter
 {
-    private String lastDirFacing = ">";
-    private int numbNeighbors;
-    private boolean stateSweep; //sweeping = true, moving forwards = false
     private String currentSweep = "LEFT";//left, right, or stay -- direction to turn
     private int spread = 1;//every spreadNumb steps, move forwards one tile
-    private int spreadNumb = 1999;
-    private int spreadNumbShift = 10;// after a while, switch to spreading more often
-    private int timeSinceInfect = 0;//how long since we've infected anyone?
+    private int spreadNumb = 1999; //wait spreadNumb steps, and then begin to spread
+    private int spreadNumbShift = 3;//once we've waited the initial 1999 steps, start waiting a much smaller unit
+                                    // of steps between spreading
+    private int timeSinceInfect = 0;//how long since we've infected anyone? used for visual display
 
+    //          getMove -- What move shall this critter make?
+    //Parameters - CritterInfo - Passes CritterInfo methods for use within getMove.
+    //Returns    - Action - which action shall this critter take?
     public Action getMove(CritterInfo info)
     {
         timeSinceInfect++;
-        
+
         if (info.getFront() == Neighbor.OTHER) {
             spread = 0;
             timeSinceInfect = 0;
@@ -34,12 +35,10 @@ public class Orca extends Critter
         if (spread%spreadNumb == 0)
         {
             spreadNumb = spreadNumbShift;
+            spreadNumbShift = (int)(1+Math.random()*10);
             spread = 0;
             return Action.HOP;
         }
-
-        //numbNeighbors = getNumbNeighbors(info);
-
 
         currentSweep = dirToSweep(info);
         if (currentSweep == "LEFT")
@@ -57,37 +56,33 @@ public class Orca extends Critter
 
 
         return Action.HOP;
-    }
+    }//end of getMove
 
+    //          getColor -- Which color shall we print this creature as?
+    //Parameters - None
+    //Returns    - Color (Returns a color on a gradient based on how long it's been since this critter infected anyone)
     public Color getColor()
     {
         timeSinceInfect = Math.min(timeSinceInfect,255);
-        return new Color((float)((timeSinceInfect)/255.0),0.0f,(float)((timeSinceInfect)/255.0));
-    }
+        return new Color((float)((255-timeSinceInfect)/255.0),0.3f,(float)((timeSinceInfect)/255.0));
+    }//end of getColor
+    
 
-    private int getNumbNeighbors(CritterInfo info)
-    {
-        int count = 0;
-        if ((info.getFront()==Neighbor.SAME )|| (info.getFront() == Neighbor.WALL))
-            count++;
-
-        if ((info.getRight()==Neighbor.SAME )|| (info.getRight() == Neighbor.WALL))
-            count++;
-
-        if ((info.getLeft()==Neighbor.SAME )|| (info.getLeft() == Neighbor.WALL))
-            count++;
-
-        if ((info.getBack()==Neighbor.SAME )|| (info.getBack() == Neighbor.WALL))
-            count++;
-
-        return count;
-    }
-
-    //          dirToFace -- what direction faces away from other members of the same species?
+    //          dirToSweep -- tells the critter how it should turn to avoid facing towards a member of its own species
     //Parameters - CritterInfo - Passes CritterInfo methods for use within getMove.
-    //Returns    - LEFT or RIGHT
+    //Returns    - LEFT, RIGHT, or STAY
     private String dirToSweep(CritterInfo info)
     {
+        if (Math.random() < 0.02D)
+        {
+            if (Math.random() < 0.5D)
+            {
+                return "LEFT";
+            }
+            return "RIGHT";
+        }
+
+
         if (currentSweep == "STAY")
         {
             if (Math.random() < 0.5D)
@@ -99,7 +94,11 @@ public class Orca extends Critter
         if ((info.getLeft() == Neighbor.SAME || info.getLeft() == Neighbor.WALL) && (info.getRight() == Neighbor.SAME || info.getRight() == Neighbor.WALL))
         {
             if (info.getFront()==Neighbor.SAME || info.getFront() == Neighbor.WALL)
+            {
+                if (Math.random() < 0.5D)
                 return "LEFT";
+                return "RIGHT";
+            }
             return "STAY";
         }
         if (currentSweep == "LEFT")
@@ -119,10 +118,15 @@ public class Orca extends Critter
         }
 
         return "STAY";
-    }
+    }//end of dirToSweep
 
+
+    //          toString -- What string should the creature be printed as?
+    //Parameters - None
+    //Returns    - String (the string to print as)
     public String toString()
     {
         return "0";
-    }
+    }//end of toString
+    
 }
